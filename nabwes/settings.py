227f0badb468f8f7afcc381e2a0e9ed0fc721b6e
Datson-bit <datonayomide@gmail.com>
2024-12-26 15:44:10 +0100

    ayo2

--- nabwes/settings.py
+++ nabwes/settings.py
@@ -85,22 +85,22 @@ WSGI_APPLICATION = 'nabwes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'dngo.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),  # Replace with your database name
-        'USER': config('DB_USER'),  # Replace with your PostgreSQL user
-        'PASSWORD': config('DB_PASSWORD'),  # Replace with your PostgreSQL password
-        'HOST': 'localhost',  # Use 'localhost' or your PostgreSQL server IP
-        'PORT': '5432',  # Default PostgreSQL port
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#       'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),  # Replace with your database name
+#         'USER': config('DB_USER'),  # Replace with your PostgreSQL user
+#         'PASSWORD': config('DB_PASSWORD'),  # Replace with your PostgreSQL password
+#         'HOST': 'localhost',  # Use 'localhost' or your PostgreSQL server IP
+#         'PORT': '5432',  # Default PostgreSQL port
+#  }
+# }
 
     # 'default': dj_database_url.config(
     #     'ENGINE': 'django.db.backends.sqlite3',
